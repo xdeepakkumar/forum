c0a34ecdf66b523a0b4278521459d24fca3c46f2
xdeepakkumar <xdeepakkumar355@gmail.com>
2021-06-13 01:48:36 +0530

    header updated

--- includes/_header.php
+++ includes/_header.php
@@ -42,7 +42,7 @@ session_start();
         <input class="form-control mr-sm-2" type="search" name="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-danger my-2 my-sm-0" type="submit">Search</button>
         <p class="text-light my-0 px-2"> Welcome <b>'.$_SESSION['user_name'].'</b></p>
-        <a href="includes/_logout.php" class="btn btn-success btn-sm ml-2">Logout</a>
+        <a href="includes/_logout.php" class="btn btn-success ml-2">Logout</a>
         </form>';
       }else{
         echo '
@@ -50,8 +50,8 @@ session_start();
               <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
               <button class="btn btn-danger my-2 my-sm-0" type="submit">Search</button>
             </form>
-            <button class="btn btn-success btn-sm ml-2"  data-toggle="modal" data-target="#loginModal">Login</button>
-            <button class="btn btn-primary btn-sm mx-2"  data-toggle="modal" data-target="#signupModal">SignUp</button>
+            <button class="btn btn-success ml-2"  data-toggle="modal" data-target="#loginModal">Login</button>
+            <button class="btn btn-primary mx-2"  data-toggle="modal" data-target="#signupModal">SignUp</button>
           '; 
       }
   echo ' </div>
